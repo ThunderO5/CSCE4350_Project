c92cc70a4c0a08d7b3198ee12f1d0697b6e4f58a
= <thunderoyebi@my.unt.edu>
2026-03-08 16:55:09 -0500

    Added the SET functionality to the program.

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 # Opens database file
-file = open("data.db", "a")
 
 # Classes
 # Key-Value pair class
 class keyValue:
-    def __int__(self, key, value):
-        self.key = key
-        self.value = value
+    def __init__(self):
+        self.store = []
+    
+    def SET(key, value):
+        file = open("data.db", "a")
+        file.write(f"{key} {value} \n")
+
+    def GET(self, cmdKey):
+        print("Hello!")
+
+kv = keyValue
 
 # Loops through system input
 while True:
-    cmd = input("Enter commands (SET, GET, EXIT): ")
-    if cmd == "EXIT":
+    line = input("Enter commands (SET, GET, EXIT): ")
+    parts = line.split(" ", 2)
+    command = parts[0]
+
+    if command == "EXIT":
         break
-    print(cmd)
+    if command == "SET":
+        key = parts[1]
+        value = parts[2]
+        kv.SET(key, value)
+
+    if command == "GET":
+        print(parts[0])
+    
+    
+    
